cadc22a33a01d328428244b8c2d66f55e34eb109
Adialia <skor.roksi@gmail.com>
2016-01-22 20:31:21 +0200

    parse parter.ua for event urls

--- scrapper.js
+++ scrapper.js
@@ -10,12 +10,25 @@ var request = require('request'),
 
 
 
-var html = 'http://vk.com/absurdpabs';
+var html = 'http://parter.ua/ua.html';
+var urls = [];
 
 request(html,function(err,resp, body){
-	if (!err && resp.statusCode === 200){
+	if (!err && resp.statusCode == 200){
 		var $ = cheerio.load(body);
-		console.log($(#public_events);
-		//#public_events.a	
+		//console.log(body);
+		$("div[class='event']").each(function(){
+			var url = $(this).find('a').attr('href');
+			urls.push(url);
+		});
+		console.log(urls);
+		//class = event
+		//a class = eventtitle, innerHtml - title
+		/*$(div.event,table).each(function(){
+			var url = attr.attr('href');
+			urls.push(url);
+		});
+		console.log(ulrs);*/
 	}
 });
+
