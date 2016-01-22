b69116eec7f2e729680d0eac081f2fc980d85daf
NorseUA <norseua@gmail.com>
2016-01-22 23:36:23 +0200

    Events generator modified

--- modules/eventsGenerator.js
+++ modules/eventsGenerator.js
@@ -1,4 +1,38 @@
 exports.generateEvents = function() {
-  var events = {'test': 'test'}
-  return events;
+  var name, date, time, place;
+  var events = {};
+  var Events = function(name, date, time, place){
+	this.name = name;
+	this.date = date;
+	this.time = time;
+	this.place = place;  		
+  }
+
+  function randomWord(size) {
+        var result = '';
+        var words = 'qwertyuiopasdfghjklzxcvbnm';
+        var maxPosition = words.length - 1;
+            for(var i = 0; i < size; i++ ) {
+                position = Math.floor ( Math.random() * maxPosition );
+                result = result + words.substring(position, position + 1);
+            }
+        return result;
+    }
+
+  function randomNumber(min, max) {
+    var rand = min + Math.random() * (max + 1 - min);
+    rand = Math.floor(rand);
+    return rand;
+  }
+	for(var j = 0; j < 100; j++){
+		name = randomWord(7);
+		date = randomNumber(1, 30) + '.' + randomNumber(1, 12) + '.' + randomNumber(2016, 2020);
+		time = randomNumber(0, 23) + ':' + randomNumber(0, 59);
+		place = randomWord(9);
+		
+		events[name] = new Events(name, date, time, place);
+	}
+		return events;
+		
+	
 };
--- routes/events.js
+++ routes/events.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/all', function(req, res, next) {
+router.get('/', function(req, res, next) {
 	var events =  require('../controllers/events');
   	res.json(events.allEvents());
 });
