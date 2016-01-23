01ebe03ed3cff4bffa91eeaee07e7b7c586a34d6
Adialia <skor.roksi@gmail.com>
2016-01-23 19:31:39 +0200

    eventTitle, eventLocation, eventDescription, eventLink from parter.ua added

--- scrapper.js
+++ scrapper.js
@@ -10,7 +10,7 @@ var request = require('request'),
 
 
 
-var html = 'http://parter.ua/ua.html';
+var html = 'http://parter.ua';
 var urls = [];
 
 request(html,function(err,resp, body){
@@ -21,14 +21,34 @@ request(html,function(err,resp, body){
 			var url = $(this).find('a').attr('href');
 			urls.push(url);
 		});
-		console.log(urls);
-		//class = event
-		//a class = eventtitle, innerHtml - title
-		/*$(div.event,table).each(function(){
-			var url = attr.attr('href');
-			urls.push(url);
-		});
-		console.log(ulrs);*/
+		//console.log(urls);
+		//urls.length
+		//for (var i = 0; i < urls.length; i++){
+		for (var i = 1; i < 2; i++){
+			//console.log(i);
+			var fullUrl = html + urls[i];
+			//console.log(fullUrl);
+			request(fullUrl,function(err,resp,body){
+				//console.log('outer url request works '+urls[i]);
+				if (!err && resp.statusCode == 200){
+					//console.log('url request works '+fullUrl);
+					var $ = cheerio.load(body,{
+						decodeEntities: false
+					});
+					var eventTitle, eventLocation, eventDescription, eventTime, eventLink;
+
+					var eventPage = $('td.center');
+					eventLink = fullUrl;
+					eventTitle = $( 'h1', eventPage).text()//.replace(/<.*>/g, '. ');
+					console.log("eventTitle",eventTitle);
+					eventLocation = $('p[class="txt"]',eventPage).text();
+					//console.log("eventLocation",eventLocation);
+					eventDescription = $('p[class="txt"]',eventPage).next().text()
+					console.log("eventDescription",eventDescription);
+
+				}
+			});
+		}
 	}
 });
 
